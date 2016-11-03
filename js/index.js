688bf918dbd80120ae8fdda37d53d71c501dd9a1
htmlzhoyan <601819456@qq.com>
2016-11-03 11:04:23 +0800

    ddddafc
    ff

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@
 	$('.swiper-container').hide()
 
 	$('#enter').tap(function(){
-	$('#mainContent').show()
+		$('#mainContent').show()
 	  $('.swiper-container').hide()
 	  //数据请求,然后请求api、skill，显示在浏览器上iScoll
 	  $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/skill.json?rand='+Math.random(),function(response){
@@ -74,7 +74,8 @@
 	     html+='<li>'+'<div class="pics"><img src='+response[i].img+'></div>'+response[i].category+'-------------'+response[i].name+'</li>'
 	    }
 	    $('#scroller ul').html(html);
-		 
+		var myScroll = new IScroll('#wrapper', {mouseWheel:true});
+	    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false); 	    
 	  })	 
 	})
 
@@ -96,10 +97,11 @@
 	}) 
 	 
 	  $('#footer div').eq(1).tap(function(){
+	  	  var html = '';
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'}) 
 	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/project.json?rand='+Math.random(),function(response){
-	      var html = '';
+	    
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="pros"><img src='+response[i].img+'></div>'+response[i].name+'-------------'+response[i].category+'</li>'
 
@@ -109,10 +111,11 @@
 	    })
 	  }); 
 	   $('#footer div').eq(0).tap(function(){
+	   	  var html = '';
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})
 	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/skill.json?rand='+Math.random(),function(response){
-	      var html = '';
+	    
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="pics"><img src='+response[i].img+'></div>'+response[i].category+'-------------'+response[i].name+'</li>'
 	      }
@@ -120,10 +123,11 @@
 	    })
 	  });  
 	  $('#footer div').eq(2).tap(function(){
+	  	   var html = '';
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})  
 	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/work.json?rand='+Math.random(),function(response){
-	      var html = '';
+	   
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+response[i].name+'</li>'
 	        html+='<li><img style="width:120px;height:40px;margin:10px" src='+response[i].img+'></li>'
