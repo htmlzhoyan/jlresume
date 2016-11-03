4c25f57dd30ed40df772cee8ca614c18efec3e4e
htmlzhoyan <601819456@qq.com>
2016-11-03 09:14:38 +0800

    fff

--- js/index.js
+++ js/index.js
@@ -68,7 +68,7 @@
 		$('#mainContent').show()
 	  $('.swiper-container').hide()
 	  //数据请求,然后请求api、skill，显示在浏览器上iScoll
-	  $.post('https://htmlzhoyan.github.io/jlresume/mock/skill.json',{},function(response){
+	  $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/skill.json',{},function(response){
 	    var html = '';
 	    for(var i=0;i<response.length;i++){
 	     html+='<li>'+'<div class="pics"><img src='+response[i].img+'></div>'+response[i].category+'-------------'+response[i].name+'</li>'
@@ -104,7 +104,7 @@
 	  $('#footer div').eq(0).tap(function(){
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})
-	    $.post('https://htmlzhoyan.github.io/jlresume/mock/skill.json',{},function(response){
+	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/skill.json',{},function(response){
 	      var html = '';
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="pics"><img src='+response[i].img+'></div>'+response[i].category+'-------------'+response[i].name+'</li>'
@@ -117,7 +117,7 @@
 	  $('#footer div').eq(1).tap(function(){
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'}) 
-	    $.post('https://htmlzhoyan.github.io/jlresume/mock/project.json',{},function(response){
+	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/project.json',{},function(response){
 	      var html = '';
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="pros"><img src='+response[i].img+'></div>'+response[i].name+'-------------'+response[i].category+'</li>'
@@ -131,7 +131,7 @@
 	  $('#footer div').eq(2).tap(function(){
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})  
-	    $.post('https://htmlzhoyan.github.io/jlresume/mock/work.json',{},function(response){
+	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/work.json',{},function(response){
 	      var html = '';
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+response[i].name+'</li>'
@@ -146,7 +146,7 @@
 	  $('#footer div').eq(3).tap(function(){
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})   
-	    $.post('https://htmlzhoyan.github.io/jlresume/mock/interest.json',{},function(response){
+	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/interest.json',{},function(response){
 	      var html = '';
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="pics"><img src='+response[i].img+'></div>'+response[i].name+'</li>'
@@ -159,7 +159,7 @@
 	  $('#footer div').eq(4).tap(function(){
 	     $('#footer div').css({'background':'#444','color':'#fff'})
 	      $(this).css({'background':'#e9e9e9','color':'#31B020'})   
-	    $.post('https://htmlzhoyan.github.io/jlresume/mock/myself.json',{},function(response){
+	    $.getJSON('https://htmlzhoyan.github.io/jlresume/mock/myself.json',{},function(response){
 	      var html = '';
 	      for(var i=0;i<response.length;i++){
 	        html+='<li>'+'<div class="mys"><img src='+response[i].img+'></div>'+response[i].name+'</li>'
